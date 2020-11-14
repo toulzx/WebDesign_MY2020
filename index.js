8b5252ee68521ae692f83f10e3eacf85418c8db4
VanPCA <1286435758@qq.com>
2020-11-15 02:05:27 +0800

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var welcomePage = document.getElementById("welcomePage");
 var waterfallPage = document.getElementById("waterfallPage");
 var detailsPage =  document.getElementById("detailsPage");
 var navBar = document.getElementById("navBar");
+var bookDetailLength = document.getElementsByClassName("bookDetail").length;
 
 function getViewPortHeight() {
      return document.documentElement.clientHeight || document.body.clientHeight;
@@ -16,3 +17,30 @@ window.onload = function() {
     waterfallPage.style.height = getViewPortHeight() + "px";
     detailsPage.style.height = getViewPortHeight() + "px";
 }
+
+//避免for循环阻塞，把函数放入主线程，但会导致一些需要效果的失效。
+for(let i=0;i<bookDetailLength;i++)
+{
+    (function(i){var tryShowDetails=setInterval(()=>{
+    if(document.getElementById("detailsPage").className.indexOf('active')>-1)
+    {
+        //这句其实可以不用，和前面的visibility没什么用一起删了就行。
+        document.getElementsByClassName("bookDetail")[i].style.visibility="visible";
+        document.getElementsByClassName("titl")[i].className+=" title";
+        document.getElementsByClassName("pictur")[i].className+=" picture";
+        document.getElementsByClassName("pictureContaine")[i].className+=" pictureContainer";
+        document.getElementsByClassName("contentContaine")[i].className+=" contentContainer";
+        document.getElementsByClassName("divideLin")[i].className+=" divideLine";
+        document.getElementsByClassName("conten")[i].className+=" content";
+        clearInterval(tryShowDetails);
+    }
+},200)})(i);
+    
+}
+
+
+
+//fullpage.js插件启用
+$(document).ready(function(){
+    $('#container').fullpage();
+});  //激活fullpage的效果，可以检查页面看效果了　
